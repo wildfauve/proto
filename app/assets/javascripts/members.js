57b7a90d18d057b032d149f841f1f181f74364ac
Wild Fauve <wild.fauve@gmail.com>
2011-09-25 23:52:41 +1300

    add click test

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -1,11 +1,11 @@
-$(function() {
-/*
-	var memurl, name;
-	memurl = $("a").attr("href");
-	name = $(".member").attr("id");
-	localStorage.setItem("name", name);
-*/
-	$('.register').click(function() {
-		alert('Handler for .click() called.');
+$(document).ready(function(){
+	$("a").click(function(event){
+		alert("Thanks for visiting!");
 	});
 });
+/*
+var memurl, name;
+memurl = $("a").attr("href");
+name = $(".member").attr("id");
+localStorage.setItem("name", name);
+*/
