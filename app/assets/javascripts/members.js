3ec10779d93f57a045e64da098e0910e1d441af1
Wild Fauve <wild.fauve@gmail.com>
2011-09-25 23:42:58 +1300

    More mucking around with jquery

---
+++ app/assets/javascripts/members.js
@@ -0,0 +1,11 @@
+$(function() {
+/*
+	var memurl, name;
+	memurl = $("a").attr("href");
+	name = $(".member").attr("id");
+	localStorage.setItem("name", name);
+*/
+	$('.register').click(function() {
+		alert('Handler for .click() called.');
+	});
+});
--- app/assets/javascripts/members.js.coffee
+++
@@ -1,5 +0,0 @@
-jQuery ->
-	memurl = $("a").attr("href")
-	name = $(".member").attr("id")
-	localStorage.setItem("name", name)
-	localStorage.setItem("memurl", memurl)
